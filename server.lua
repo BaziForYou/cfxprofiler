b1c7f01b168086994c6b2695116be279c81ff7d9
NoShitSherlockium <80778117+NoShitSherlockium@users.noreply.github.com>
2023-05-05 12:09:47 -0500

    Added discord webhook support

--- server.lua
+++ server.lua
@@ -1,3 +1,8 @@
+Webhook = "https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN" -- replace with your own webhook URL
+UseWebhook = false -- set to true to send to discord webhook
+
+
+--Don't mess with anything below this line
 local ProfRunning = false
 RegisterConsoleListener(function(channel, message)
     if string.find(message, "server thread hitch warning") then
@@ -13,9 +18,11 @@ RegisterConsoleListener(function(channel, message)
     if string.find(message, "Save complete") then
         oldnamets = 0
         oldname = None
+        warnmessage = "The following resources are using more tick time than recommended, please check them out:"
+        discordmessage = warnmessage .. "\n"
         json2 = LoadResourceFile(GetCurrentResourceName(), "profiler.json")
         traceEvents = json.decode(json2).traceEvents
-        print("The following resources are using more tick time than recommended, please check them out:")
+        print(warnmessage)
         for k,v in pairs(traceEvents) do
             if string.find(v.name, "tick") then
                 if oldname == v.name then
@@ -26,11 +33,15 @@ RegisterConsoleListener(function(channel, message)
                         name = string.gsub(name, "%)", "")
                         name = string.gsub(name, " ", "")
                         print(name .. ": " .. ticktime / 1000 .. "ms")
+                        discordmessage = discordmessage .. name .. ": " .. ticktime / 1000 .. "ms\n"
                     end
                 end
                 oldname = v.name
                 oldnamets = v.ts
             end
+        end
+        if UseWebhook then
+            PerformHttpRequest(Webhook, function(err, text, headers) end, 'POST', json.encode({content = discordmessage}), { ['Content-Type'] = 'application/json' })
         end
         ProfRunning = false
     end
